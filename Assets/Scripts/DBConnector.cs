e6b8e70f4566aac0c364ec9cfe6a435461bc7f39
Ötvös Máté <mati9606@gmail.com>
2019-11-19 18:25:23 +0100

    Basic SQLLite datrabase management added... for some reason dont yet work in standalone version

---
+++ Assets/Scripts/DBConnector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Data.SQLite;
+using System.IO;
+
+public class DBConnector : MonoBehaviour
+{
+    private SQLiteConnection connection;
+
+    void Start()
+    {
+        string DBPath = Path.Combine(Directory.GetParent(Application.dataPath).ToString(), "DataBases", "mydb.db");
+        if (!Directory.Exists(Directory.GetParent(DBPath).ToString()))
+        {
+            Directory.CreateDirectory(Directory.GetParent(DBPath).ToString());
+        }
+        bool DBExist = File.Exists(DBPath);
+
+        connection = new SQLiteConnection($@"Data Source={DBPath};Version=3;");
+        connection.Open();
+
+        if (!DBExist)
+        {
+            SQLiteCommand command = connection.CreateCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = "CREATE TABLE IF NOT EXISTS 'highscores' ( " +
+                              "  'id' INTEGER PRIMARY KEY, " +
+                              "  'name' TEXT NOT NULL, " +
+                              "  'score' INTEGER NOT NULL" +
+                              ");";
+
+            command.ExecuteNonQuery();
+        }
+    }
+
+    void OnDestroy()
+    {
+        connection.Close();
+    }
+}
---
+++ Assets/Scripts/DDOL.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DDOL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);        
+    }
+
+    private void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
